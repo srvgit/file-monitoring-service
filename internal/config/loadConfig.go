8bb9093042ff67db5bab05a8779fc3289e312b7b
Srujan V <repgit@gmail.com>
2024-01-24 20:21:39 -0600

    config changes

--- internal/config/loadConfig.go
+++ internal/config/loadConfig.go
@@ -1,3 +1,32 @@
 package config
 
-//TODO move config structure here 
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	SrcDirectory    string `json:"sourceDirectory"`
+	TargetDirectory string `json:"ReportDirectory"`
+	MaxGoRoutines   int    `json:"maxGoRoutines"`
+}
+
+type ConfigProcessor interface {
+	LoadConfig(path string) (Config, error)
+}
+
+func LoadConfig(path string) (config *Config, err error) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	config = &Config{}
+	err = decoder.Decode(config)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
